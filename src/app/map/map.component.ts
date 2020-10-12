dbb4f3f6d32fe45f98b92460666e1badec50f285
worawitboat <worawit.pa.61@ubu.ac.th>
2020-10-13 01:19:05 +0700

    map2

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 declare const L: any;
 @Component({
@@ -7,43 +8,89 @@ declare const L: any;
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-  constructor() { }
+  constructor(private location: Location) { }
 
 
   ngOnInit() {
+    let type = history.state.type;
+
+
     if (!navigator.geolocation) {
       console.log('location is not supported');
     }
     const food = [
       {
-        name:"355 steak",
+        name: "355 de steak",
         latlng: [15.107047999999998, 104.91347519999999],
-        img:"",
-        des:""
+        img: "https://d1sag4ddilekf6.cloudfront.net/compressed/items/3-CZEGNP3ATNW3VX-CZEJLNE1G7KXC2/photo/menueditor_item_8a51584a46ea43c5b5e48f7d5d5458ff_1592631004188706026.jpg",
+        des: "เมนู สเต๊ก, ข้าว, ของทานเล่น"
       },
       {
-        name:"xxx",
+        name: "Mee jalern",
         latlng: [15.107247999999998, 104.92377519999999],
-        img:"",
-        des:""
+        img: "",
+        des: ""
 
       },
       {
-        name:"yyy",
+        name: "Basen",
         latlng: [15.107547999999998, 104.91357519999999],
-        img:"",
-        des:""
+        img: "https://scontent.fubp1-1.fna.fbcdn.net/v/t31.0-8/23213102_2046312892269683_7104564255025435699_o.jpg?_nc_cat=111&_nc_sid=8bfeb9&_nc_eui2=AeGuPqsKjeeoQ6a3jniJbf0cENenlvX-J4oQ16eW9f4nitdE-YG01SULoR6oW87LZ5v4uH9St8wu8Hggsq_0H1Z6&_nc_ohc=BGRdW8BcCnYAX9Wk6dd&_nc_ht=scontent.fubp1-1.fna&oh=5e4393758590aca638421e5fcf304b38&oe=5FA82E15",
+        des: ""
       },
+    ]
 
+    const noodle = [
+      {
+        name: "Basen",
+        latlng: [15.107047999999998, 104.91347519999999],
+        img: "https://scontent.fubp1-1.fna.fbcdn.net/v/t31.0-8/23213102_2046312892269683_7104564255025435699_o.jpg?_nc_cat=111&_nc_sid=8bfeb9&_nc_eui2=AeGuPqsKjeeoQ6a3jniJbf0cENenlvX-J4oQ16eW9f4nitdE-YG01SULoR6oW87LZ5v4uH9St8wu8Hggsq_0H1Z6&_nc_ohc=BGRdW8BcCnYAX9Wk6dd&_nc_ht=scontent.fubp1-1.fna&oh=5e4393758590aca638421e5fcf304b38&oe=5FA82E15",
+        des: ""
+      },
+      {
+        name: "patthai bigP",
+        latlng: [15.107247999999998, 104.92377519999999],
+        img: "",
+        des: ""
+
+      },
+      {
+        name: "Go deng",
+        latlng: [15.107547999999998, 104.91357519999999],
+        img: "",
+        des: ""
+      },
+    ]
+
+    const drink = [
+      {
+        name: "came me",
+        latlng: [15.108047999999998, 104.91447519999999],
+        img: "",
+        des: ""
+      },
+      {
+        name: "three first",
+        latlng: [15.103247999999998, 104.95377519999999],
+        img: "",
+        des: ""
+
+      },
+      {
+        name: "monkey",
+        latlng: [15.107947999999998, 104.92157519999999],
+        img: "",
+        des: ""
+      },
     ]
 
     navigator.geolocation.getCurrentPosition((position) => {
       const coords = position.coords;
       const latLong = [coords.latitude, coords.longitude];
-      const latLong2 = [15.107047999999998, 104.91347519999999];
       console.log(
         `lat: ${position.coords.latitude}, lng: ${position.coords.longitude}`
       );
+      
       let mymap = L.map('map').setView(latLong, 13);
 
       L.tileLayer(
@@ -60,18 +107,54 @@ export class MapComponent implements OnInit {
       ).addTo(mymap);
 
       let marker = L.marker(latLong).addTo(mymap);
-      for(let i = 0;i < food.length ; i++){
-        let store = L.marker(food[i].latlng).addTo(mymap);
-        store.bindPopup('<div id="content">' +
-        '<div id="siteNotice">' +
-        "</div>" +
-        '<center>' +
-        '<img src="https://lh3.googleusercontent.com/p/AF1QipNkmwigiRz6uZeQm14ORdxc-0y2FktUK_izG6j5=w600-k" alt="Girl in a jacket" width="240" height="150">' +
-        '<center/>' +
-        '<h1 id="firstHeading" class="firstHeading">'+food[i].name+'</h1>' +
-        '<div id="bodyContent">' +
-        "</div>" +
-        "</div>").openPopup();
+      if (type == 'food') {
+        for (let i = 0; i < food.length; i++) {
+          let store = L.marker(food[i].latlng).addTo(mymap);
+          store.bindPopup(
+            '<div id="content">' +
+            '<div id="siteNotice">' +
+            "</div>" +
+            '<center>' +
+            '<img src="' + food[i].img + '"alt="Girl in a jacket" width="240" height="150">' +
+            '<center/>' +
+            '<h1 id="firstHeading" class="firstHeading">' + food[i].name + '</h1>' +
+            '<div id="bodyContent">' +
+            '<h4>' + food[i].des + '</h4>' +
+            "</div>" +
+            "</div>").openPopup();
+        }
+      } else if (type == 'noodle') {
+        for (let i = 0; i < noodle.length; i++) {
+          let store = L.marker(noodle[i].latlng).addTo(mymap);
+          store.bindPopup(
+            '<div id="content">' +
+            '<div id="siteNotice">' +
+            "</div>" +
+            '<center>' +
+            '<img src="' + noodle[i].img + '"alt="Girl in a jacket" width="240" height="150">' +
+            '<center/>' +
+            '<h1 id="firstHeading" class="firstHeading">' + noodle[i].name + '</h1>' +
+            '<div id="bodyContent">' +
+            '<h4>' + noodle[i].des + '</h4>' +
+            "</div>" +
+            "</div>").openPopup();
+        }
+      } else if (type == 'drink') {
+        for (let i = 0; i < drink.length; i++) {
+          let store = L.marker(drink[i].latlng).addTo(mymap);
+          store.bindPopup(
+            '<div id="content">' +
+            '<div id="siteNotice">' +
+            "</div>" +
+            '<center>' +
+            '<img src="' + drink[i].img + '"alt="Girl in a jacket" width="240" height="150">' +
+            '<center/>' +
+            '<h1 id="firstHeading" class="firstHeading">' + drink[i].name + '</h1>' +
+            '<div id="bodyContent">' +
+            '<h4>' + drink[i].des + '</h4>' +
+            "</div>" +
+            "</div>").openPopup();
+        }
       }
 
 
